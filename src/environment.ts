ce0170f3f58a120acb624d6ccb5ee3b102078e01
Mostafa Elshikh <youremail@example.com>
2025-12-20 10:38:57 -0800

    Use Railway Private Networking to bypass CORS and public domain issues

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://back-end-production-6523.up.railway.app/api',  // الصحيح 100%
-  baseUrl: 'https://back-end-production-6523.up.railway.app',
+  apiUrl: 'http://back-end:8080/app/api',  // الصحيح 100%
+  baseUrl: 'http://back-end:8080/app',
   appName: 'MECAL',
 };
